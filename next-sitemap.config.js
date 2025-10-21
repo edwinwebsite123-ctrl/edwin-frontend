7f48ff55aec884d1a2af93c381c968e6f418ea0b
muhsinmuhsy <muhsinkoodali856@gmail.com>
2025-10-22 01:12:55 +0530

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.edwinacademy.com',
+  generateRobotsTxt: true, // (optional) generate robots.txt
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
